dfb55691609b86d81c39050cd7a4abb65049e34e
Vaishnavi S <vaishnavi0324fsd@gmail.com>
2024-12-14 19:28:14 +0530

    Update AppDownload.jsx

--- src/components/AppDownload/AppDownload.jsx
+++ src/components/AppDownload/AppDownload.jsx
@@ -5,7 +5,9 @@ import { assets } from '../../assets/assets'
 const AppDownload = () => {
   return (
     <div className = 'app-download' id = 'app-download'>
-      <p>For Betterr Experience Download <br /> Tomato App</p>
+      <p>
+        For Betterr Experience Download <br /> Tomato App
+      </p>
       <div className = 'app-download-platforms'>
         <img src = {assets.play_store} alt = ''/>
         <img src = {assets.app_store} alt = ''/>
