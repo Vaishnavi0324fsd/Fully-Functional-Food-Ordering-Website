e872aa7fd16b9ca2e5f4b4bdec09f20587ace0dd
Vaishnavi S <vaishnavi0324fsd@gmail.com>
2024-12-14 19:31:41 +0530

    Update ExploreMenu.jsx

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -6,14 +6,18 @@ const ExploreMenu = ({category,setCategory}) => {
 
   return (
     <div className = 'explore-menu' id = 'explore-menu'>
-      <h1>Explore our menu</h1>
-      <p className = 'explore-menu-text'>Choose from a diverse menu features a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience.</p>
+      <h1>
+        Explore our menu
+      </h1>
+      <p className = 'explore-menu-text'>
+        Choose from a diverse menu features a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience.
+      </p>
       <dev className = 'explore-menu-list'>
         {menu_list.map((item,index)=>{
             return(
                 <div onClick = {() => setCategory(prev => prev === item.menu_name ? "All" : item.menu_name)}key = {index} className = 'explore-menu-list-item'>
                     <img className = {category === item.menu_name ? 'active' : ''}src = {item.menu_image} alt = ''/>
-                    <p>{item.menu_name}</p>
+                    <p>{ item.menu_name }</p>
                 </div>
             )
         })}
